cd9be13e2f61ecea7055bc11b8b087e4aa725536
sunxiaozhi <1129535445@qq.com>
2018-01-09 16:45:04 +0800

    add new file

---
+++ math.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"math"
+)
+
+func main() {
+	fmt.Println("My favorite number is", rand.Intn(10))
+	fmt.Println(math.Pi)
+}
---
+++ var.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+var c, python, java bool
+
+func main() {
+	var i int
+	fmt.Println(i, c, python, java)
+}
+
+/*var 语句定义了一个变量的列表；跟函数的参数列表一样，类型在后面。
+
+就像在这个例子中看到的一样，`var` 语句可以定义在包或函数级别。*/
+
+
+/*package main
+
+import "fmt"
+
+var i, j int = 1, 2
+
+func main() {
+	var c, python, java = true, false, "no!"
+	fmt.Println(i, j, c, python, java)
+}*/
+
+/*
+变量定义可以包含初始值，每个变量对应一个。
+
+如果初始化是使用表达式，则可以省略类型；变量从初始值中获得类型。*/
